525ded7d31f35f236cbfa1c321e5b58376512a05
Tan Changde <tanchangde@gmail.com>
2024-01-10 19:43:48 +0800

    feat(cn_company)!: delete test files and related functions

--- tests/testthat/test-lxr_cn_company_candlestick.R
+++
@@ -1,9 +0,0 @@
-test_that("Basic Functionality", {
-  result <- lxr_cn_company_candlestick(
-    type = "lxr_fc_rights",
-    start_date = "2023-01-03",
-    end_date = "2024-01-03",
-    stock_code = "300750"
-  )
-  expect_s3_class(result, "tbl_df")
-})
--- tests/testthat/test-lxr_cn_company_equity_change.R
+++
@@ -1,8 +0,0 @@
-test_that("Basic Functionality", {
-  result <- lxr_cn_company_equity_change(
-    start_date = "2022-09-20",
-    limit = 1314,
-    stock_code = "002352"
-  )
-  expect_s3_class(result, "tbl_df")
-})
