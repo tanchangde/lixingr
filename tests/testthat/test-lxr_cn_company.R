1c79003ee8c8a9e5b71a71de45b9e9e0d9a20bdd
Tan Changde <tanchangde@gmail.com>
2024-01-11 07:49:04 +0800

    test: add test for "cn_company_divide"

--- tests/testthat/test-lxr_cn_company.R
+++ tests/testthat/test-lxr_cn_company.R
@@ -15,3 +15,9 @@ test_that("Missing Parameters Should Return Error", {
   expect_error(lxr_query(api_endpoint = lxr_cn_company_equity_change(),
     start_date = "2022-09-20", limit = 1314))
 })
+
+test_that("Test cn_company_dividend", {
+  result <- lxr_query(api_endpoint = lxr_cn_company_dividend(),
+  start_date = "2023-01-11", end_date = "2024-01-11", stock_code = "300750")
+  expect_s3_class(result, "tbl_df")
+})
