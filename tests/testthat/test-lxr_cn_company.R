bd7afb59de1e1de3979c25f1f081275cb6b80571
Tan Changde <tanchangde@gmail.com>
2024-01-11 12:50:26 +0800

    test: add test for "cn_company_fs_non_financial"

--- tests/testthat/test-lxr_cn_company.R
+++ tests/testthat/test-lxr_cn_company.R
@@ -25,3 +25,10 @@ test_that("test cn_company_dividend", {
   start_date = "2023-01-11", end_date = "2024-01-11", stock_code = "300750")
   expect_s3_class(result, "tbl_df")
 })
+
+test_that("test cn_company_fs_non_financial", {
+  result <- lxr_query(api_endpoint = lxr_cn_company_fs_non_financial(),
+  date = "2022-09-30", stock_codes = c("300750", "600519", "600157"),
+  metrics_list = c("q.ps.toi.t"))
+  expect_s3_class(result, "tbl_df")
+})
