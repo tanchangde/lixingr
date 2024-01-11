af82d3787d0d522d3694897dd71321830790eedc
Tan Changde <tanchangde@gmail.com>
2024-01-11 08:03:20 +0800

    test(cn_company): refactor test and simple prompt

--- tests/testthat/test-lxr_cn_company.R
+++ tests/testthat/test-lxr_cn_company.R
@@ -1,9 +1,3 @@
-test_that("retrieve specified stock information from the cn_company interface", {
-  result <- lxr_query(api_endpoint = lxr_cn_company(),
-  stock_codes = c("300750", "600519", "600157"))
-  expect_s3_class(result, "tbl_df")
-})
-
 test_that("invalid parameters should return error", {
   expect_error(lxr_query(api_endpoint = lxr_cn_company_dividend(),
   invalid_params = "invalid"))
@@ -12,6 +6,12 @@ test_that("invalid parameters should return error", {
 test_that("missing parameters should return error", {
   expect_error(lxr_query(api_endpoint = lxr_cn_company_equity_change(),
     start_date = "2022-09-20", limit = 1314))
+})
+
+test_that("test cn_company", {
+  result <- lxr_query(api_endpoint = lxr_cn_company(),
+  stock_codes = c("300750", "600519", "600157"))
+  expect_s3_class(result, "tbl_df")
 })
 
 test_that("test cn_company_dividend", {
