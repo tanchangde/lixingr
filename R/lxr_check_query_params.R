8fec1326606bd4df88db151e297d5cba681414af
Tan Changde <tanchangde@gmail.com>
2024-01-12 13:16:35 +0800

    refactor: rename function name and params

--- R/lxr_check_query_params.R
+++ R/lxr_check_query_params.R
@@ -6,25 +6,30 @@
 #' parameters, it will stop the function and return an error message.
 #'
 #' @param query_params A named list of query parameters.
-#' @param valid_params A character vector of valid parameter names.
-#' @param required_params A character vector of required parameter names.
+#' @param endpoint_valid_params A character vector of valid parameter names.
+#' @param endpoint_required_params A character vector of required parameter
+#'   names.
 #'
 #' @return This function does not return a value. If the check is successful,
 #'   the function will complete silently. If there are invalid or missing
 #'   parameters, the function will stop and return an error message.
-lxr_check_params <- function(query_params, valid_params, required_params) {
-  if (!all(names(query_params) %in% valid_params)) {
-    invalid_params <- setdiff(names(query_params), valid_params)
+lxr_check_query_params <- function(
+    query_params, endpoint_valid_params, endpoint_required_params) {
+  if (!all(names(query_params) %in% endpoint_valid_params)) {
+    invalid_params <- setdiff(names(query_params), endpoint_valid_params)
     message <- glue::glue(
       "Invalid parameters: {paste(invalid_params, collapse = ', ')} \n",
-      "Valid parameters are: {paste(valid_params, collapse = ', ')}"
+      "Valid parameters are: {paste(endpoint_valid_params, collapse = ', ')}"
     )
     usethis::ui_stop(message)
   }
 
-  if (!is.null(required_params) &&
-    !all(required_params %in% names(query_params))) {
-    missing_required_params <- setdiff(required_params, names(query_params))
+  if (!is.null(endpoint_required_params) &&
+    !all(endpoint_required_params %in% names(query_params))) {
+    missing_required_params <- setdiff(
+      endpoint_required_params,
+      names(query_params)
+    )
     message <- glue::glue(
       "Missing required parameters: ",
       "{paste(missing_required_params, collapse = ', ')}."
