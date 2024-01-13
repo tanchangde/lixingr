baa245fa892229c286c48ec8c0d4cbcb450c1d02
Tan Changde <tanchangde@gmail.com>
2024-01-13 19:44:49 +0800

    docs: clear document description for lxr_endpoint_name()

--- R/lxr_endpoint_name.R
+++ R/lxr_endpoint_name.R
@@ -1,11 +1,11 @@
-#' Extract the API name from a Lixinger API URL
+#' Extract the endpoint name
 #'
-#' This function takes a Lixinger API URL as input, and returns the API name,
-#' which is the part of the URL after "api/". All slashes ("/") in the API name
-#' are replaced with underscores ("_").
+#' This function takes a Lixinger endpoint URL as input, and returns the
+#' endpoint name, which is the part of the URL after "api/". All slashes ("/")
+#' in the endpoint name are replaced with underscores ("_").
 #'
-#' @param endpoint A string of the Lixinger API URL.
-#' @return A string of the API name.
+#' @param endpoint A string of the Lixinger endpoint URL.
+#' @return A string of the endpoint name.
 #'
 #' @importFrom magrittr extract2
 lxr_endpoint_name <- function(endpoint) {
