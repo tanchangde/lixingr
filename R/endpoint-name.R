368eaca9e6b3465df54942e51fa23e74face2b2e
tanchangde <tanchangde@gmail.com>
2024-02-09 12:10:00 +0800

    docs: clear expression

--- R/endpoint-name.R
+++ R/endpoint-name.R
@@ -1,10 +1,12 @@
 #' Extract the endpoint name
 #'
-#' This function takes a Lixinger endpoint URL as input, and returns the
-#' endpoint name, which is the part of the URL after "api/". All slashes ("/")
-#' in the endpoint name are replaced with underscores ("_").
+#' This function takes a lixinger endpoint URL as input. It first extracts the
+#' part of the URL after "api/". Then, all slashes ("/") and hyphens ("-") in
+#' this part are replaced with underscores ("_"). The result is returned as the
+#' endpoint name.
 #'
 #' @param endpoint A string of the Lixinger endpoint URL.
+#'
 #' @return A string of the endpoint name.
 lxr_endpoint_name <- function(endpoint) {
   endpoint_name <- stringr::str_match(endpoint, "api/(.*$)") %>%
